5a7b55a856e3b2642d372f64fde26de0bb84dc12
JordySoliz21 <127996721+JordySoliz21@users.noreply.github.com>
2023-04-26 08:29:40 -0600

    Add files via upload

---
+++ Class26april/unoal100_main.java
@@ -0,0 +1,13 @@
+package Class26april;
+
+public class unoal100_main {
+
+	public static void main(String[] args) {
+		int num=1;
+	while(num<=100) {
+	System.out.println(num);
+	num++;
+
+	}
+	}
+}
